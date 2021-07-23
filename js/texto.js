2da70857fd796babac1245eeb34db47e7af6efba
ajtc16 <ajtc16@gmail.com>
2021-07-23 01:46:23 -0500

    Add files via upload

---
+++ js/texto.js
@@ -0,0 +1,91 @@
+var data = {
+    "english": 
+    {
+      "inicio_solucion1":"The <b>solution</b>",
+      "inicio_solucion2":"that your bussines",
+      "inicio_solucion3":"<b>needs</b>",
+      "inicio_labrido": "We seek to help our clients overcome all of their obstacles through the search of operational efficiency, digital transformation, innovation and the implementation of solutions that are result driven, lower costs and increase margins. Our solutions will allow your business model to continuously adapt to innovation and technological changes.",
+      "inicio_gente_tech_titulo":"Tech People",
+      "inicio_gente_tech_texto":"Onboard we are fortunate to have a group of high pedigree developers, engineers, architects covering all aspects of technology development and implementation. They are committed to client needs, to making themselves available at all times and offer the most suited tech according to a company’s reality.",
+      "inicio_seguridad_titulo":"Security",
+      "inicio_seguridad_texto":"We offer tech solutions at the highest level without taking our eyes from what’s most important- security of your data. We never let our sight of the sensitive information of your company so that you feel safe against any real threat.",
+      "inicio_ecosistema_titulo":"Ecosystem",
+      "inicio_ecosistema_texto":"We work as an innovative platform that integrates the use of tech tools, strategic relationships that offer industry-specific expertise and the development of executive education resources. This combination allows the client to receive a robust solution that goes beyond the implementation of new technology, it is paired with the proper training of their staff to maximize the use of the acquired tech.",
+             
+      "lab_service":"our service. A catalyst of change that companies need most based on their strategic objectives: Innovation, Digital, Mar-keting, Corporate, Global and Social.",
+      "lab_digital_titulo":"LAB= Digital Innovation",
+      "lab_digital_texto":"By offering strategic tech based on CBM (Component Business Model) in industries like consumer goods, retail, agricul-ture, insurance and many others, it allows us to offer a comprehensive solutions scheme unique to each industry. System-izing operations, processes, functions; implementing complex IT systems.<br/><br/>We are aware of what the current market offering is to  companies from consulting firms. Our offering is not transaction-based with our clients but rather a long term relationship. Our clients have endless needs and we have solutions for them in: Digital Transformation,  Data and Analytics, ERP, MRP, Business Intelligence, Cloud and Cyber Security.<br/><br/> Our diverse track record and backgrounds in our core group in addition to our strategic relationships allow us to provide a distinct offering in several industries: Banking, Insurance, Retail, Education, Consumer Goods to name a few. ",
+      
+      "lab_tech_education_titulo":"LAB= Tech Ed.",
+      "lab_tech_education_texto":"Our division of Human Capital our tech solutions involve the pairing of an education with a focus on the human capital of a company. We believe that the tech acquired by our clients can not be fully put to use if the workforce of the company is not properly trained to use it. Our curriculum has a main focus on: required skill sets for the implementation and use of software tools, leadership, team building, culture and wellness engineering.",
+
+      "lab_process_titulo":"Process",
+      "lab_process_texto":"Tech People, Security and Ecosystem is our formula. These three tools are our conversation starter to our clients. Our ethos is that tech shouldn’t be imposed on companies, but rather, a thorough analysis to have real-time pain points and needs. As a result we accurately offer solutions to reduce costs and time taking the following steps:",
+
+      "lab_process1_titulo":"Company Diagnosis:",
+      "lab_process1_texto":"we get to know you, we speak with different company divisions and their people. The in-formation gathered allows us to identify tech  and operational needs.",
+
+      "lab_process2_titulo":"Create a solution:",
+      "lab_process2_texto":"one that can be adopted by the company using the most suited software and hardware tools with the client’s budget in mind. Report: we itemize the capital costs, time and human efficiencies our services will represent the company.",
+
+      "lab_process3_titulo":"Report: ",
+      "lab_process3_texto":"we itemize the capital costs, time and human efficiencies our services will represent the company.",
+
+      "lab_process4_titulo":"Implementation:",
+      "lab_process4_texto":"upon client approval, we execute the solutions.",
+
+      "lab_process5_titulo":"Training:",
+      "lab_process5_texto":"all key personnel of the company that will use the solutions implemented will receive the proper training. We also create video manuals for future staff to have these documents available.",
+     
+      "ideas_inicio":"<span style=\"color:#52D6FC; \">Welcome</span><span style=\"color:black; \"> to our Ideas page. Here you can be part of our Tech People platform. This is an open space where you can register and be part of the open and free dialogue to share innovation ideas you wish to see in your company. Be a service, a tool, equipment, etc. We want the community to reach out to us and discuss emerging technologies like AI, IoT, AR/VR, Blockchain among others.</span>",
+      
+      "nosotros_jhon":"John is an entrepreneur from New York City with 20 years experience in Real Estate, technology and other industries.<br/><br/> John has served in executive positions under several strategies in investment banking, company restructurings, and private equity throughout his career. John performed Managing Director duties at Luna Advisors, advising institutional investors from South Korea in the allocation of capital in core assets in the U.S.<br/><br/>  Today he continues to offer his experience and his strategic relationships by creating a bridge between investment opportunities and capital seeking diversification to their portfolio in different part of the world across several industries like consumer goods, real estate, finance and technology. <br/><br/>  Currently he is part of the Capital Markets division at Southport Compass based in CT offering debt solutions to companies and high net-worth individuals. ",
+      "nosotros_andrea":"Andrea has executed projects in innovation and technology in IBM Cloud - Personality Insight to increase clients revenue, by implementing VR she was able to increase the client’s customer base.<br/><br/> This was with the help creating proof of concept ideas in a 24-hour period using low-code platforms. The implementation of these projects were able to optimize telework with the use of Microsoft 365; generating demand in tech companies, creating data bases through the use of strategic alliances, using UX to establish a Netsuite CRM for companies.<br/><br/> Andrea was in Spain generated industry leading ideas to innovate training in aerospace. While in the U.S. she was able to work on projects related to blockchain.",
+        
+    },
+    "espanol": 
+    {
+      "inicio_solucion1":"La <b>solución</b>",
+      "inicio_solucion2":"que tu empresa",
+      "inicio_solucion3":"<b>necesita</b>",
+      "inicio_labrido": "Nosotros queremos acompañar a nuestros clientes a resolver sus obstáculos a través de la búsqueda de la eficiencia operacional, la transformación digital, innovación e implementación de soluciones para que obtengan mejores resultados disminuyendo los costos e incrementando las ganancias. Nuestra asesoría y soluciones hace que su modelo de negocios se adapte continuamente a la innovación tecnológica en su respectiva industria.",
+      "inicio_gente_tech_titulo":"Gente Tech",
+      "inicio_gente_tech_texto":"Contamos con un grupo de expertos  desarrolladores, ingenieros, y arquitectos en todos los ámbitos tecnológicos. Ellos se asegurarán de estar al tanto de sus necesidades, estar disponibles, y brindar las opciones tecnológicas que mejor se adapten a su realidad.",
+      "inicio_seguridad_titulo":"Seguridad",
+      "inicio_seguridad_texto":"Ofrecemos soluciones tecnológicas de alto nivel y eficiencia, sin descuidar el tema de seguridad; de esta forma, nos aseguraremos que la información sensitiva de su empresa esté protegida ante cualquier amenaza.",
+      "inicio_ecosistema_titulo":"Ecosistema",
+      "inicio_ecosistema_texto":"Trabajamos como una plataforma innovadora que integra herramientas de tecnología, relaciones estratégicas que ofrecen expertise y mejor practica específicamente a tipo de industria, al igual que desarrollo  de recursos educación ejecutiva. Esta combinación permite a el cliente recibir una serie de soluciones robustas que van mas allá de adquirir nueva tecnología, ya que es acoplada con el entrenamiento apropiado a sus empleados para maximizar el uso de todas las soluciones que necesiten implementar.",
+      
+      "lab_service":"Nuestro servicio LAB= es el catalizador al cambio que las empresas necesitan basado en las siguientes objetivos que nuestros clientes tienen en las distintas estrategias: Innovación, Digital, Marketing, Corporativa, Global y Social.",
+      "lab_digital_titulo":"LAB= Inovación Digital",
+      "lab_digital_texto":"Nuestra oferta al mercado es tecnología estrategia basada en análisis e investigación (Modulacion de Plano) en industrias como consumo masivo, retail, agricultura, seguros, banca, educación entre otras. Esta función nos permite ofrecer un esquema de soluciones únicas por industria atacando los desafíos propios de cada industria. Sistematizando operaciones, procesos y tareas; llevando a cabo la implementación de sistemas complejos de TI.  <br/><br/> Nuestro ofrecimiento no es transaccional, mas bien queremos tener una relación a largo plazo con nuestros clientes. Las necesidades son varias y tenemos las soluciones en: Transformación Digital, Data y Analítica, ERP, MRP, Business Intelligence, Cloud y Cyber Seguridad.",
+      
+      "lab_tech_education_titulo":"LAB= Educación Tech",
+      "lab_tech_education_texto":"En nuestra division de Capital Humano nuestras soluciones tecnológicas involucran un acompañamiento educativo enfocado en el capital humano de la empresa, ya que consideramos que nuestros clientes no lograrán ser más eficientes si no existe la capacitación y preparación de los colaboradores que van a ser los usuarios principales de estas nuevas herramientas tecnológicas. Nuestras capacitaciones se enfocan principalmente las áreas de: habilidades requeridas en la implementación de los proyectos, liderazgo, equipos, cultura y ingeniería de bienestar.",
+
+      "lab_process_titulo":"Proceso",
+      "lab_process_texto":"La formula de Gente, Seguridad y Ecosistema son nuestras tres herramientas elementales para que empecemos a dialogar con su empresa. Nuestra ética profesional es de que los nuevos productos tecnológicos no deben ser impuestos a las empresas, al contrario, consideramos que debe haber un análisis de por medio que permita identificar las verdaderas dolencias y necesidades de las empresas para optimizar costos y tiempo; por esta razón nuestra forma de trabajar con las empresas está compuesta por los siguientes pasos:",
+
+      "lab_process1_titulo":"Diagnóstico empresarial:",
+      "lab_process1_texto":"Nos interesa conocer a la empresa, conversar con distintos departamentos de la empresa para que de esta forma encontrar las falencias tecnológicas y operacionales. ",
+
+      "lab_process2_titulo":"Creación de solución:",
+      "lab_process2_texto":"Que se adapte a la empresa; tomando en consideración las herramientas más eficientes y el presupuesto del cliente. ",
+
+      "lab_process3_titulo":"Reporte: ",
+      "lab_process3_texto":" Enfocándonos en los ahorros que vamos a generarle a la empresa en términos de capital, tiempo y gestión humana.",
+
+      "lab_process4_titulo":"Implementación:",
+      "lab_process4_texto":"De la solución en la empresa. ",
+
+      "lab_process5_titulo":"Capacitación:",
+      "lab_process5_texto":"Al personal del área correspondiente en los productos implementados; además para los futuros recursos dejamos manuales de uso, para que desde el día 1 reciban una inducción de las herramientas tecnológicas.",
+     
+      "ideas_inicio":" <span style=\"color:#52D6FC; \">Bienvenido</span><span style=\"color:black; \"> a nuestra pagina de Ideas. Aqui puedes ser parte de nuestra plataforma Gente Tech. Este es un espacio abierto donde te puedes registrar y ser parte de un dialogo abierto y libre para compartir ideas de innovación que desees ver en tu empresa. Ya sea un servicio, una herramienta, equitación (hardware), etc. Queremos que la comunidad se nos acerque y dialogue con nosotros de tecnologías emergentes como Inteligencia Artificial, Internet of Things, Realidad Aumentada/Realidad Virtual, Blockchain entre otras.</span>", 
+      "nosotros_jhon":"John es un emprendedor de la ciudad de Nueva York con 20 años de experiencia en bienes raíces, tecnología y otras industrias. <br/> <br/>  John se ha desempeñado en puestos ejecutivos en varias estrategias en banca de inversión, reestructuraciones de empresas y capital riesgo a lo largo de su carrera. John desempeñó funciones de Director General en Luna Advisors, asesorando inversores de Corea del Sur en la asignación de capital en activos básicos en EE. UU. <br/> <br/> Hoy continúa ofreciendo su experiencia y sus relaciones estratégicas creando un puente entre las oportunidades de inversión y la búsqueda de capital. diversificación de su cartera en diferentes partes del mundo a través de varias industrias como bienes de consumo, bienes raíces, finanzas y tecnología. <br/> <br/> Actualmente forma parte de la división de Mercados de Capitales en Southport Compass con sede en CT ofreciendo soluciones de deuda a empresas y personas de alto patrimonio.",
+      "nosotros_andrea":"Andrea ha ejecutado proyectos de innovación y tecnología en IBM Cloud - Personality Insight para aumentar los ingresos de los clientes, al implementar la realidad virtual, pudo aumentar la base de clientes del cliente. <br/> <br/> Esto fue con la ayuda de la creación de ideas de prueba de concepto en un período de 24 horas utilizando plataformas de código bajo. La implementación de estos proyectos logró optimizar el teletrabajo con el uso de Microsoft 365; generando demanda en empresas de tecnología, creando bases de datos mediante el uso de alianzas estratégicas, utilizando UX para establecer un Netsuite CRM para empresas. <br/> <br/> Andrea estuvo en España generando ideas líderes en la industria para innovar la formación en aeroespacial. Mientras estuvo en los EE. UU., Pudo trabajar en proyectos relacionados con blockchain.",
+
+      "description": 
+          "Сәбіз Lorem ipsum, жеңілдік. Бұл ауырсыну үшін осы ұннан таңдамаңыз, сондықтан аз Осы ауырсынуды орындаңыз. Қызметтер жоқ және оның айырмашылығын ұлы ыңғайсыздық таңдау, тәжірибе ретінде қабылданған ештеңе өңдеу үшін нәтиже инцидент және қателіктерді зерттеуші ләззат, өмір, encounter born сияқты үлкен тағамдар_бәрі! Осы мәселе бойынша біздің сабоның кейбірін ашу, зерттеуші дұрыс! Дана, біз asperiores туған қызметтер тізімі деді олардың жұмыс, кез келген уақытта, содан бері үлкен жүгірістер, соның ішінде ләззат немесе рахат өңдеу сұраймыз. Қашуға."
+    }
+  }
